9a8c3c44cb9ebc737394608bd0df704f1d0bd703
Pedro Lemos <lemozxpedro@gmail.com>
2023-09-17 15:14:33 -0300

    Alphabet is now UTF-8

--- lib/abstractions/constants.dart
+++ lib/abstractions/constants.dart
@@ -1,35 +1,9 @@
 const epsilon = 'ε';
 const sigma = 'Σ';
-const alphabet = {
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-  '.',
-  ' ',
-  '"',
-  ',',
-  '-',
-};
+Set<String> get alphabet {
+  final utf8 = <String>{};
+  for (var i = 0; i < 127; i++) {
+    utf8.add(String.fromCharCode(i));
+  }
+  return utf8;
+}
