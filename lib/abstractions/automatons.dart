f4f10aa8466947d8b722a8b7ede567eaa184c41e
Pedro Lemos <lemozxpedro@gmail.com>
2024-09-07 20:56:13 -0300

    Created a getter for adjacencyMatrix

--- lib/abstractions/automatons.dart
+++ lib/abstractions/automatons.dart
@@ -20,6 +20,32 @@ sealed class Automaton<StateType extends State> {
   }
 
   bool evaluate(String input);
+
+  List<List<int>> get adjacencyMatrix {
+    final matrix = <List<int>>[];
+
+    for (var idxA = 0; idxA < states.length; idxA++) {
+      final row = <int>[];
+
+      final stateA = states.elementAt(idxA);
+      final transitions = switch (this) {
+        DeterministicAutomaton() => stateA.transitions.values,
+        NonDeterministicAutomaton() =>
+          stateA.transitions.values.expand((values) => values),
+      };
+
+      for (var idxB = 0; idxB < states.length; idxB++) {
+        final stateB = states.elementAt(idxB);
+        final quantity = transitions.where((state) => state == stateB).length;
+
+        row.add(quantity);
+      }
+
+      matrix.add(row);
+    }
+
+    return matrix;
+  }
 }
 
 abstract class DeterministicAutomaton extends Automaton<DeterministicState> {
